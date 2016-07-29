d6a352166eeead3eda1221b06efe6f5d0cd80077
leverie <leverie623+GitHub@gmail.com>
2016-07-30 08:27:02 +0900

    임시
    ㅇ

--- Client/UserInterface.cpp
+++ Client/UserInterface.cpp
@@ -4,18 +4,47 @@
 
 CUserInterface::CUserInterface()
 {
+	m_pVertexBuffer = nullptr;
+	m_pIndexBuffer = nullptr;
+
+	m_pTexture = nullptr;
+}
+CUserInterface::CUserInterface(const CUserInterface& ref)
+{
 }
 CUserInterface::~CUserInterface()
 {
 	Destroy();
 }
 
-void CUserInterface::Initialize()
+void CUserInterface::Initialize(ID3D11Device *pd3dDevice, int screenWidth, int screenHeight, int bitmapWidth, int bitmapHeight)
 {
+	// 화면 크기를 저장한다
+	m_iScreenWidth = screenWidth;
+	m_iScreenHeight = screenHeight;
+
+	// 비트맵이 그려져야 하는 곳의 픽셀을 저장한다
+	m_iBitmapWidth = bitmapWidth;
+	m_iBitmapHeight = bitmapHeight;
+
+	// 이전 렌더 위치를 -1로 초기화
+	m_iPreviousPosX = -1;
+	m_iPreviousPosY = -1;
+
+	//// 버텍스버퍼와 인덱스버퍼를 초기화한다
+	//InitializeBuffers(device);
+
+	//WCHAR* textureFilename
+	//// Load the texture for this model.
+	//LoadTexture(pd3dDevice, textureFilename);
 }
 
 void CUserInterface::Destroy()
 {
+	ReleaseTexture();
 }
 
+void CUserInterface::Render(ID3D11DeviceContext *pd3dDeviceContext)
+{
 
+}
--- Client/UserInterface.h
+++ Client/UserInterface.h
@@ -1,19 +1,47 @@
 #ifndef CUSERINTERFACE_H_
 #define CUSERINTERFACE_H_
-
+#include "Object.h"
 
 
 class CUserInterface
 {
 public:
 	CUserInterface();
+	CUserInterface(const CUserInterface&);
 	~CUserInterface();
 
-	void Initialize();
+	void Initialize(ID3D11Device *pd3dDevice, int screenWidth, int screenHeight, int bitmapWidth, int bitmapHeight);
 	void Destroy();
 
+	void Render(ID3D11DeviceContext *pd3dDeviceContext);
+
+	int GetIndexCount();
+	ID3D11ShaderResourceView* GetTexture();
+
+
+
+	/* pos + tex = CDiffusedVertex */
 protected:
 private:
+	ID3D11Buffer *m_pVertexBuffer;
+	ID3D11Buffer *m_pIndexBuffer;
+
+	int m_iVertexSize;
+	int m_iIndexSize;
+
+	CTexture *m_pTexture;
+
+	int m_iScreenWidth;
+	int m_iScreenHeight;
+	int m_iBitmapWidth;
+	int m_iBitmapHeight;
+
+	int m_iPreviousPosX;
+	int m_iPreviousPosY;
+
+	void Update();
+	void LoadTexture();
+	void ReleaseTexture();
 };
 
 
